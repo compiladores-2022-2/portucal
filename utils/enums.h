61ad7413a2607c480b07f45a7d41526844e14349
Unknown <camila.duarte.018@A303-25L.imd.local>
2022-08-29 10:29:28 -0300

    initial lexical rules

---
+++ utils/enums.h
@@ -0,0 +1,29 @@
+#ifndef _TOKEN_ID_ENUM
+#define _TOKEN_ID_ENUM
+
+enum TOKEN_ID {
+  DIG,
+  ALPHA,
+  ID,
+  NUM_FLUT,
+  NUM_INT,
+  CONTINUE,
+  PARE,
+  SE,
+  SENAO,
+  ENQUANTO,
+  FACA,
+  PARA,
+  ESCOLHA,
+  CASO,
+  RETORNE,
+  LEIA,
+  IMPRIMA,
+  VAR,
+  INT,
+  DOIS_PONTOS,
+  PONTO_VIRG,
+  ERRO
+};
+
+#endif
